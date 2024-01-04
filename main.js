8283dd8c236ef2343588e00ad6c60a77e26ad322
Azamat Rasulov <propalsradarov23@gmail.com>
2024-01-05 00:16:01 +0500

    added title

---
+++ main.js
@@ -0,0 +1,8 @@
+(function () {
+  // создаём и возвращаем заголовок приложения
+  function createAppTitle(title) {
+    let appTitile = document.createElement("h2");
+    appTitile.innerHTML = title;
+    return appTitile;
+  }
+});
