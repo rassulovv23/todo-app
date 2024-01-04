c0f58481bbd21928357c58922336e9ee72fa96e5
Azamat Rasulov <propalsradarov23@gmail.com>
2024-01-05 00:36:27 +0500

    added form

--- main.js
+++ main.js
@@ -5,4 +5,29 @@
     appTitile.innerHTML = title;
     return appTitile;
   }
+
+  // создаём и возвращаем форму для создания дела
+  function createItemForm() {
+    let form = document.createElement("form");
+    let input = document.createElement("input");
+    let buttonWrapper = document.createElement("div");
+    let button = document.createElement("button");
+
+    form.classList.add("input-group", "mb-3");
+    input.classList.add("form-control");
+    input.placeholder = "Введите название нового дела";
+    buttonWrapper.classList.add("input-group-append");
+    button.classList.add("btn", "btn-primary");
+    button.textContent = "Добавить дело";
+
+    buttonWrapper.append(button);
+    form.append(input);
+    form.append(buttonWrapper);
+
+    return {
+      form,
+      input,
+      button,
+    };
+  }
 });
